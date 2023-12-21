19cbcda07ad381f658f6c17036a70dfc5c7e8a55
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-12-21 08:15:24 +0100

    feat(index): rename

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +12,12 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1 className="m-2">Mundwerk PDF Tools</h1>
+      <Link
+        className="rounded-md p-1 bg-orange-400 text-slate-950 cursor-pointer"
+        to="./fax-composer/client"
+      >
+        zu den PDF tools
+      </Link>
     </div>
   );
 }
