c4f83d0fe2fd0c33ae9ef9c074324b664e1920a1
Stefan Huckschlag <stefan.huckschlag@web.de>
2024-07-13 18:30:28 +0200

    feat(pdf): handle multiline content

--- app/utils/pdf.ts
+++ app/utils/pdf.ts
@@ -1,4 +1,9 @@
-import { PDFDocument, StandardFonts } from "pdf-lib";
+import {
+  PDFDocument,
+  StandardFonts,
+  TextAlignment,
+  layoutMultilineText,
+} from "pdf-lib";
 
 export type PdfAttachment = {
   bytes: ArrayBuffer;
@@ -7,12 +12,22 @@ export type PdfAttachment = {
   creationDate: string;
   modificationDate: string;
 };
-async function createPdf(
-  sender: string,
-  recepient: string,
-  pdfAttachment: PdfAttachment,
-  content: string
-) {
+
+export type CreatePdfProps = {
+  sender: string;
+  recipient: string;
+  recipientNumber: string;
+  pdfAttachment: PdfAttachment;
+  content: string;
+};
+
+async function createPdf({
+  sender,
+  recipient,
+  recipientNumber,
+  pdfAttachment,
+  content,
+}: CreatePdfProps) {
   const pdfAttachmentBytes = pdfAttachment?.bytes;
   const fontSize = 20;
   try {
@@ -41,18 +56,38 @@ async function createPdf(
       size: fontSize,
       font: timesRomanFont,
     });
-    page.drawText(recepient, {
+    page.drawText(recipient, {
       x: 100,
       y: height - 6 * fontSize,
       size: fontSize,
       font: timesRomanBoldFont,
     });
-    page.drawText(content, {
-      x: 50,
-      y: height - 8 * fontSize,
+    page.drawText(recipientNumber, {
+      x: 100,
+      y: height - 7 * fontSize,
       size: fontSize,
-      font: timesRomanFont,
+      font: timesRomanBoldFont,
     });
+    const multiLineContent = layoutMultilineText(content, {
+      alignment: TextAlignment.Left,
+      font: timesRomanFont,
+      fontSize,
+      bounds: {
+        x: 50,
+        y: height - 8 * fontSize,
+        width: 500,
+        height: 500,
+      },
+    });
+    multiLineContent.lines.forEach((line, index) => {
+      page.drawText(line.text, {
+        x: 50,
+        y: height - 10 * fontSize - index * fontSize * 1.2,
+        size: fontSize,
+        font: timesRomanFont,
+      });
+    });
+    console.log(multiLineContent);
     // Add the PDF attachment
     if (pdfAttachmentBytes) {
       const pdfToEmbed = await PDFDocument.load(pdfAttachmentBytes);
@@ -63,6 +98,45 @@ async function createPdf(
         pdfDoc.addPage(pages[i]);
       }
     }
+    const result = {
+      doc: await pdfDoc.save(),
+      base64string: await pdfDoc.saveAsBase64(),
+    };
+    return result;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
+async function createEticketPdf(ticketDocBytes: ArrayBuffer) {
+  try {
+    const pdfDoc = await PDFDocument.create();
+    // Add the PDF attachment
+    if (ticketDocBytes) {
+      const ticketDoc = await PDFDocument.load(ticketDocBytes, {
+        ignoreEncryption: true,
+      });
+      const pageCount = ticketDoc.getPageCount();
+      const pages = [];
+      for (let i = 0; i < pageCount; i++) {
+        if (i === 0 || i === 3) {
+          pages.push(pdfDoc.addPage());
+        }
+        const ticket = await pdfDoc.embedPage(ticketDoc.getPages()[i], {
+          left: 55,
+          bottom: 485,
+          right: 300,
+          top: 575,
+        });
+        const ticketDimensions = ticket.scale(1);
+        const page = pages[pages.length - 1];
+        page.drawPage(ticket, {
+          ...ticketDimensions,
+          x: page.getWidth() / 2 - ticketDimensions.width / 2,
+          y: page.getHeight() / 2 - ticketDimensions.height / 2 - 50,
+        });
+      }
+    }
     const result = await pdfDoc.save();
     return result;
   } catch (error) {
@@ -71,4 +145,4 @@ async function createPdf(
   }
 }
 
-export { createPdf };
+export { createPdf, createEticketPdf };
