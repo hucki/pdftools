a30b839e385bcdcbe3f890fed21bfef5595ee21c
Stefan Huckschlag <stefan.huckschlag@web.de>
2024-08-09 09:57:42 +0200

    feat(CoverPageForm): remove sender input

--- app/components/organisms/CoverPageForm.tsx
+++ app/components/organisms/CoverPageForm.tsx
@@ -1,16 +1,16 @@
 type CoverPageFormProps = {
   sender: string;
+  senderNumber: string;
   recipientName: string;
   onChange: (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => void;
-  recipientNumber: string;
-  recipientNumberMatch: boolean;
   content: string;
 };
 
 export const CoverPageForm = ({
   sender,
+  senderNumber,
   recipientName,
   onChange,
   content,
@@ -18,16 +18,9 @@ export const CoverPageForm = ({
   return (
     <div className="p-4 bg-slate-100 border border-dashed border-spacing-1 grid grid-rows-1gap-2">
       <h2 className="text-xl mb-2">Deckblatt ausfüllen</h2>
-      <label className="text-slate-500">
-        Von (Name):{" "}
-        <input
-          className="border rounded-md p-1 w-full text-black"
-          type="text"
-          name="sender"
-          value={sender}
-          onChange={onChange}
-        />
-      </label>
+      <p className="text-slate-500">
+        Von: {sender} ({senderNumber} )
+      </p>
       <label className="text-slate-500">
         An (Name):{" "}
         <input
