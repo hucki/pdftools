234ba37a124bfc2c8ee20cefc6cf140fb259be09
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-01-18 00:09:14 +0100

    feat(Container): add style props

--- app/components/atoms/Container.tsx
+++ app/components/atoms/Container.tsx
@@ -2,11 +2,23 @@ import { ReactNode } from "react";
 
 interface ContainerProps {
   children: ReactNode;
+  bg?: string;
+  className?: string;
+  rounded?: string;
+  shadow?: string;
 }
 
-export const Container = ({ children }: ContainerProps) => {
+export const Container = ({
+  children,
+  className,
+  bg = "bg-white",
+  rounded = "rounded-md",
+  shadow = "shadow-md",
+}: ContainerProps) => {
   return (
-    <div className="p-4 bg-white rounded-md shadow-md grid gap-2 auto-rows-max">
+    <div
+      className={`p-4 ${bg} ${rounded} ${shadow} grid gap-2 auto-rows-max ${className}`}
+    >
       {children}
     </div>
   );
