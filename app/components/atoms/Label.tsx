f82574c468612392c248928ca83f9e6336c65539
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-01-18 00:06:36 +0100

    refactor(Label): move into FormElements

--- app/components/atoms/Label.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export const Label = ({ children }: { children: ReactNode }) => {
-  return (
-    <label className="text-slate-500 italic text-xs m-1">{children}</label>
-  );
-};
