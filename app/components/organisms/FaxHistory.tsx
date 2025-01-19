960cfd536d1be7f5c08fc83a1da86ca5f81ae8dc
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-01-19 01:14:00 +0100

    feat(FaxHistory): fix styling

--- app/components/organisms/FaxHistory.tsx
+++ app/components/organisms/FaxHistory.tsx
@@ -2,7 +2,6 @@ import { useRouteLoaderData } from "@remix-run/react";
 import { LoaderResult } from "../../routes/fax";
 import { FaxHistoryItem } from "../../utils/history";
 import { FaxHistoryItemList } from "./FaxHistoryItemList";
-import { Container } from "../atoms/Container";
 
 export default function FaxHistory() {
   const loaderData = useRouteLoaderData<LoaderResult>("routes/fax");
@@ -14,19 +13,23 @@ export default function FaxHistory() {
   }
 
   return (
-    <Container>
-      <h2 className="text-xl font-bold">Fax Ausgang</h2>
-      <div className="flex flex-col text-xs">
-        <FaxHistoryItemList
-          items={historyItems.filter((item) => item.direction === "OUTGOING")}
-        />
+    <div className="bg-white p-4 flex flex-col h-full overflow-hidden rounded-md shadow-md">
+      <div className="h-1/2 overflow-hidden flex flex-col">
+        <h2 className="text-xl font-bold">Fax Ausgang</h2>
+        <div className="h-full overflow-y-auto text-xs shadow-inner bg-gray-50 rounded-md">
+          <FaxHistoryItemList
+            items={historyItems.filter((item) => item.direction === "OUTGOING")}
+          />
+        </div>
       </div>
-      <h2 className="text-xl font-bold mt-4">Fax Eingang</h2>
-      <div className="flex flex-col text-xs">
-        <FaxHistoryItemList
-          items={historyItems.filter((item) => item.direction === "INCOMING")}
-        />
+      <div className="h-1/2 overflow-hidden flex flex-col">
+        <h2 className="text-xl font-bold mt-4">Fax Eingang</h2>
+        <div className="h-full overflow-y-auto text-xs shadow-inner bg-gray-50 rounded-md">
+          <FaxHistoryItemList
+            items={historyItems.filter((item) => item.direction === "INCOMING")}
+          />
+        </div>
       </div>
-    </Container>
+    </div>
   );
 }
