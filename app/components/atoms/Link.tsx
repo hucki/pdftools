75d9e19d5afb5a756153b0b35f0931d086bba2d6
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-03-10 20:33:14 +0100

    feat(Link): add rest parameter

--- app/components/atoms/Link.tsx
+++ app/components/atoms/Link.tsx
@@ -6,11 +6,12 @@ type LinkProps = {
   to: string;
   children?: ReactNode;
 };
-export const Link = ({ to, className, children }: LinkProps) => {
+export const Link = ({ to, className, children, ...rest }: LinkProps) => {
   return (
     <RemixLink
       className={`grid grid-cols-[1fr_2fr] px-2 py-1 m-1 text-xs items-center bg-blue-100 rounded hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${className}`}
       to={to}
+      {...rest}
     >
       {children}
     </RemixLink>
