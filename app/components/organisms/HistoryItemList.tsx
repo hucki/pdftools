0dd38446cdf500c1a8325a697d289682ba529b51
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-03-10 20:34:26 +0100

    refactor(HistoryList): split components

--- app/components/organisms/HistoryItemList.tsx
+++ app/components/organisms/HistoryItemList.tsx
@@ -29,6 +29,7 @@ export const HistoryItemList = ({ items }: { items: HistoryItem[] }) => {
             {new Date(item.lastModified).toLocaleString("de-DE")}
           </span>
         </div>
+
         <div className="flex flex-col">
           {(item.direction === "INCOMING" ||
             item.direction === "MISSED_INCOMING") && (
@@ -43,25 +44,37 @@ export const HistoryItemList = ({ items }: { items: HistoryItem[] }) => {
             </span>
           )}
         </div>
+        {item.note && (
+          <span className="text-xs text-gray-500 ml-2">{item.note}</span>
+        )}
         {item.type === "VOICEMAIL" && item.recordingUrl && (
           <AudioPlayer src={item.recordingUrl} />
         )}
         <div className="flex items-center">
-          {item.read === true ? (
-            <DocumentCheckIcon className="h-5 w-5 text-green-600" />
-          ) : (
-            <DocumentCheckIcon className="h-5 w-5 text-gray-500" />
-          )}
-          {item.archived === true ? (
-            <ArchiveBoxIcon className="h-5 w-5 text-green-600" />
-          ) : (
-            <ArchiveBoxIcon className="h-5 w-5 text-gray-500" />
-          )}
-          {item.starred === true ? (
-            <StarIcon className="h-5 w-5 text-orange-300" />
-          ) : (
-            <StarIcon className="h-5 w-5 text-gray-500" />
-          )}
+          <DocumentCheckIcon
+            title={item.read === true ? "Gelesen" : "Ungelesen"}
+            aria-label={item.read === true ? "Gelesen" : "Ungelesen"}
+            className={`h-5 w-5 ${
+              item.read === true ? "text-green-600" : "text-gray-500"
+            }`}
+          />
+
+          <ArchiveBoxIcon
+            title={item.archived === true ? "Archiviert" : "Nicht archiviert"}
+            aria-label={
+              item.archived === true ? "Archiviert" : "Nicht archiviert"
+            }
+            className={`h-5 w-5 ${
+              item.archived === true ? "text-amber-800" : "text-gray-500"
+            }`}
+          />
+          <StarIcon
+            title={item.starred === true ? "Favorit" : "Nicht Favorit"}
+            aria-label={item.starred === true ? "Favorit" : "Nicht Favorit"}
+            className={`h-5 w-5 ${
+              item.starred === true ? "text-yellow-400" : "text-gray-500"
+            }`}
+          />
         </div>
       </div>
     );
