31cef4758e2f193bccfae5643d6698a8b3c0f95a
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-03-10 20:33:32 +0100

    feat(archive): new route

---
+++ app/routes/archive.tsx
@@ -0,0 +1,25 @@
+import { getHistoryLoader } from "../utils/history";
+import { Navigation } from "../components/organisms/Navigation";
+import { useLoaderData } from "@remix-run/react";
+import { HistoryView } from "../components/organisms/HistoryView";
+
+export const loader = getHistoryLoader({ archived: true });
+
+export default function Archive() {
+  const { callsIncoming, callsMissed, callsOutgoing, voicemails } =
+    useLoaderData<typeof loader>();
+  return (
+    <div className="flex md:h-full">
+      <Navigation width="w-2/12 md:w-1/12" />
+      <div className="bg-slate-100 h-full w-10/12 md:w-11/12">
+        <HistoryView
+          callsIncoming={callsIncoming}
+          callsMissed={callsMissed}
+          callsOutgoing={callsOutgoing}
+          voicemails={voicemails}
+          isArchive={true}
+        />
+      </div>
+    </div>
+  );
+}
